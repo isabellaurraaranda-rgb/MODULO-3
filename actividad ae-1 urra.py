746f404e482f540d68b34bd773a14801d87e673a
isabellaurraaranda-rgb <isabellaurraaranda@gmail.com>
2026-02-11 21:56:35 -0300

    Add files via upload

---
+++ actividad ae-1 urra.py
@@ -0,0 +1,12 @@
+# Solicitar las notas al usuario
+nota1 = int(input("Ingrese la primera nota: "))
+nota2 = int(input("Ingrese la segunda nota: "))
+nota3 = int(input("Ingrese la tercera nota: "))
+
+# Calcular el promedio
+promedio = (nota1 + nota2 + nota3) / 3
+
+# Mostrar el resultado
+print("El promedio de las notas es:", promedio)
+
+
